dca172cfa33172493afb60ab4f32fe78687e9f65
Osmar Liera <osmar2001lg@gmail.com>
2024-09-05 01:48:14 -0700

    transitions

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,17 @@
 const companyName = "Company Name";
 const companyLogoPath = "/favicon.svg";
 const companyNumber = "+526151559659";
+const companyEmail = "hola@companyname.com";
+const companyAddress =
+  "Hilario Perez 152, Revolucion II, 23075, La Paz, México";
+const companyAddressLink = `https://www.google.com/maps?q=${encodeURIComponent(companyAddress)}`;
 
 export const config = {
   companyName,
   companyNumber,
+  companyEmail,
+  companyAddress,
+  companyAddressLink,
   companyLogo: {
     path: companyLogoPath,
     alt: companyName,
